02ba26fd61735301424e8516c34e16e5e3a8825e
Bárbara Cristina Blank Garibaldi <barbarag@paripassu.com.br>
2019-07-28 20:35:37 -0300

    operation persistence DB

---
+++ Api/src/main/java/com/testestockinfo/teste/Controller/OperationController.java
@@ -0,0 +1,59 @@
+package com.testestockinfo.teste.Controller;
+
+import com.testestockinfo.teste.Model.Account;
+import com.testestockinfo.teste.Model.Deposit;
+import com.testestockinfo.teste.Model.Operation;
+import com.testestockinfo.teste.Repository.AccountRepository;
+import com.testestockinfo.teste.Model.Withdraw;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/operacao")
+public class OperationController {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Account> get() {
+        return accountRepository.findAll();
+    }
+
+    @RequestMapping(value = "/extrato/{id}", method = RequestMethod.GET)
+    public List<Operation> accountStatement(@PathVariable("id") int idAccount) throws Exception {
+        Account account = accountRepository.findById(idAccount).orElseThrow(() -> new Exception("Conta não encontrada"));
+        List<Operation> statements = new ArrayList<>();
+
+        statements.addAll(account.getDepositList());
+        statements.addAll(account.getWithdrawList());
+        //TODO ordernar por DATA e ID
+        return statements;
+    }
+
+    @RequestMapping(value = "/deposito/{id}", method = RequestMethod.POST)
+    public Account deposit(@PathVariable("id") int idAccount, @RequestBody Deposit request) throws Exception {
+        Account account = accountRepository.findById(idAccount).orElseThrow(() -> new Exception("Conta não encontrada"));
+
+        Deposit deposit = new Deposit();
+        deposit.setValue(request.getValue());
+        account.deposit(deposit);
+
+        return accountRepository.save(account);
+    }
+
+    @RequestMapping(value = "/saque/{id}", method = RequestMethod.POST)
+    public Account withdraw(@PathVariable("id") int idAccount, @RequestBody Withdraw request) throws Exception {
+        Account account = accountRepository.findById(idAccount).orElseThrow(() -> new Exception("Conta não encontrada"));
+
+        Withdraw withdraw = new Withdraw();
+        withdraw.setValue(request.getValue());
+        account.withdraw(withdraw);
+
+        return accountRepository.save(account);
+    }
+}
---
+++ Api/src/main/java/com/testestockinfo/teste/Model/Operation.java
@@ -0,0 +1,18 @@
+package com.testestockinfo.teste.Model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Operation {
+
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
